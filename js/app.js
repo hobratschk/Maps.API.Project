4dd2765767deb63714dfb88e3889ac65972cd340
Hobratschk <hobratschkbl@gmail.com>
2018-03-02 08:42:42 -0600

    made marker bounce 5 times on click

--- js/app.js
+++ js/app.js
@@ -30,7 +30,7 @@ function initMap() {
     {title: 'Open Air Museum', location: {lat: 64.118852, lng: -21.816203}},
     {title: 'Micro Bar', location: {lat: 64.148594, lng: -21.94075399999997}},
     {title: 'Eyja Guldsmeden Hotel', location: {lat: 64.142033, lng: -21.907347}},
-    {title: 'Hote Kriunes', location: {lat: 64.088630, lng: -21.794383}},
+    {title: 'Hotel Kriunes', location: {lat: 64.088630, lng: -21.794383}},
     {title: 'Room With a View', location: {lat: 64.145732, lng: -21.930194}},
     {title: 'Discover Iceland Private Tours ', location: {lat: 64.127738, lng: -21.81591400000002}}
   ];
@@ -51,6 +51,9 @@ function initMap() {
     })
     //push marker to array of markers
     markers.push(marker);
+    marker.addListener ('click', function() {
+      toggleBounce(this);
+    });
     //create onclick event to open an infowindow at each marker
     marker.addListener('click', function() {
       populateInfoWindow(this, largeInfowindow);
@@ -66,9 +69,24 @@ function initMap() {
       infowindow.open(map, marker);
       //make sure marker property is cleared if infowindow is closed
       infowindow.addListener('closeclick', function(){
-        infowindow.setMarker(null);
+        infowindow.setContent(null);
       });
     }
   }
+
+//function toggles the bounce, and the setTimout piece limits to approx 5 bounces
+//each bounce approx 700 ms
+//source: https://stackoverflow.com/questions/7339200/bounce-a-pin-in-google-maps-once
+  function toggleBounce(marker) {
+    if (marker.getAnimation() != null) {
+      marker.setAnimation(null);
+    } else {
+      marker.setAnimation(google.maps.Animation.BOUNCE);
+      setTimeout(function() {
+        marker.setAnimation(null);
+      }, 3500);
+    }
+  }
 }
+
 //ko.applybindings(new ViewModel());
